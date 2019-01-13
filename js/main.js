ac54d3372971043dc551b27b80a82bc951b3f7bd
Cezar <cezar_pm@hotmail.com>
2019-01-13 01:51:00 -0200

    add prompt

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 var botao = document.getElementById("botao");
 
 
-function spamar (){
+function spamar (paginas){
 
-    for(var i = 0; i < 100; i++){
+    for(var i = 0; i < paginas; i++){
         
         window.open("https://www.google.com")
    
     }
 }
 
-botao.addEventListener("click",spamar)
+var resposta = prompt("Responda 50 + 50");
+
+botao.addEventListener("click",spamar);
+
+if(resposta == 50){
+
+    alert("Voce acertou!!!");
+
+} else{
+      
+    spamar(resposta);  
+
+}
