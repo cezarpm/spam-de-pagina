ca589355599255372482d5579303d574291aa2b3
Cezar <cezar_pm@hotmail.com>
2019-01-13 01:44:40 -0200

    upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var botao = document.getElementById("botao");
+
+
+function spamar (){
+
+    for(var i = 0; i < 100; i++){
+        
+        window.open("https://www.google.com")
+   
+    }
+}
+
+botao.addEventListener("click",spamar)
